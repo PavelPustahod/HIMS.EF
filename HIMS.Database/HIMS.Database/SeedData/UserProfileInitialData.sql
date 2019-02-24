e82c5d114a65e4e1c9145b22fad7bd4f5cc8e213
maximsan <maximuzz@yandex.ru>
2019-02-24 23:01:46 +0300

    Add scripts for initial data generation

---
+++ HIMS.Database/HIMS.Database/SeedData/UserProfileInitialData.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[UserProfile]
+		([DirectionId], [Name], [Email], [LastName], [Sex], [Education], [BirthDate], [UniversityAverageScore], [MathScore], [Address], [MobilePhone], [Skype], [StartDate])
+	VALUES
+		(1, 'Alex', 'alex@m.ru', 'Al', 'M', 'BNTU', '01-07-2000', 7.7, 8.2, 'Pr. Pobedi 38-19', '+37529324546', 'maximilian', '01-01-2019')
