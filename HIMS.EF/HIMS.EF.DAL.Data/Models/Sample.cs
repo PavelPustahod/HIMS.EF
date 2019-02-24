4888849c9982f10a140da6e16a694cd94cd088a6
maximsan <maximuzz@yandex.ru>
2019-02-24 23:20:25 +0300

    Add sample, userProfile models

---
+++ HIMS.EF/HIMS.EF.DAL.Data/Models/Sample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.EF.DAL.Data.Models
+{
+    public class Sample
+    {
+        public int SampleId { get; set; }
+
+        public string Description { get; set; }
+
+        public string Name { get; set; }
+    }
+}
