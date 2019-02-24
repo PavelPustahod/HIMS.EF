dd44e6afbf068ab43cbd594b9362dbff594569fb
maximsan <maximuzz@yandex.ru>
2019-02-24 23:47:46 +0300

    Add identity models, repository, interface IUnitOfWork

---
+++ HIMS.EF/HIMS.EF.DAL.Identity/Models/UserSecurityManager.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIMS.EF.DAL.Identity.Models
+{
+    public class UserSecurityManager : UserManager<ApplicationUser>
+    {
+        public UserSecurityManager(IUserStore<ApplicationUser> store) : base(store)
+        {
+        }
+    }
+}
